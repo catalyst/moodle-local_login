55d2fbbd2990a202a6b0243af04f5891cf9d2d95
Dan Marsden <dan@danmarsden.com>
2022-06-30 09:43:12 +1200

    bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_login';
-$plugin->release = '2022062800';
-$plugin->version = 2022062800;
+$plugin->release = '2022063000';
+$plugin->version = 2022063000;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
