34e1a8ce9345a2f90e19b5d53964ef0f240df02d
Sumaiya Javed <sumaiya.javed@catalyst.net.nz>
2022-08-30 15:27:53 +1200

    Version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_login';
-$plugin->release = '2022070500';
+$plugin->release = '2022070501';
 $plugin->version = 2022070501;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
