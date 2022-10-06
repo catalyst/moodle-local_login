56488eb47f611487d1ef225818fc49ce70c01d29
Dan Marsden <dan@danmarsden.com>
2022-10-06 14:09:32 +1300

    Fix #10 - don't redirect if guest user.

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ require_once('../../config.php');
 require_once($CFG->dirroot.'/login/lib.php');
 require_once($CFG->libdir.'/authlib.php');
 
-if (isloggedin()) {
+if (isloggedin() && !isguestuser()) {
     // User already logged in.
     $urltogo = core_login_get_return_url();
     unset($SESSION->wantsurl);
